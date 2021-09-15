939ca53627247680f51c24a995d161ac5542893b
Damir <dadoka84@hotmail.com>
2021-09-15 15:31:14 +0200

    first commit

---
+++ index2.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Geotehnika doo</title>
+<link href="css.css" rel="stylesheet" type="text/css" />
+<meta name="keywords" content="miniranje, miner, bušenje, eksploziv, produbljenje, more, podmorski, podmorsko, podvodno, podvodna, seizmička" />
+<script src="/js-global/FancyZoom.js" type="text/javascript"></script>
+<script src="/js-global/FancyZoomHTML.js" type="text/javascript"></script>
+</head>
+
+
+<body onload="setupZoom()">
+<div class="wrap">
+  <table width="940" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td align="right" valign="bottom" class="header">&nbsp;</td>
+    </tr>
+    <tr>
+      <td class="main"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="256" align="center" valign="top" ><table width="256" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td class="bgr"><div class="poslovi">PODMORSKA MINIRANJA </div></td>
+              </tr>
+            <tr>
+              <td class="bgr"><div class="poslovi">PODMORSKI ISKOPI</div></td>
+              </tr>
+            <tr>
+              <td class="bgr"><div class="poslovi">BUŠENJA I MINIRANJA NA KOPNU</div></td>
+              </tr>
+          </table>            </td>
+          <td width="684" align="center" valign="middle"><?php include 'gal2.html'; ?>
+            
+            
+            
+            </td>
+        </tr>
+      </table></td>
+    </tr>
+    <tr>
+      <td><br />
+        <h1>
+        Dobrodošli na stranicu GEOTEHNIKA Umag</h1>
+        <p>Geotehnika d.o.o. je poduzeće koje je osnovano 2002. godine u gradu Umagu. Naša djelatnost je vrlo specifična, a odnosi se na hidrogradnju, specijalna podmorska miniranja i iskope u moru.<br />
+          <br />
+        Za sve dodatne informacije slobodno nas nazovite putem našeg kontakt telefona ili e-pošte.</p>
+      <p>&nbsp;</p></td>
+    </tr>
+    <tr>
+      <td><div class="infot">
+        <div class="text">
+          <h1>Adresa:</h1>
+          Tribje 26, <br>
+52470 Umag, Hrvatska<br />
+<br />
+Tel: +385(0)52-741160<br />
+<br />
+Direktor: +385(0)98-9394674<br />
+        </div>
+        <div class="text2">
+          <h1>Email:</h1>
+          <a href="mailto:geoumag@gmail.com">geoumag@gmail.com</a></div>
+        <div class="mapa"></div>
+        <iframe width="475" height="190" frameborder="0" scrolling="No" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=13.517700433731079%2C45.4262718416104%2C13.529287576675415%2C45.43305576696261&amp;layer=mapnik&amp;marker=45.429660141542854%2C13.523494005203247" style="border: 0px solid black"></iframe>
+      </div></td>
+    </tr>
+    <tr>
+      <td align="center" valign="middle"><div class="siva"></div>
+      <div class="foottext">Geotehnika  d.o.o. Umag| Tribje 26 | 52470 Umag | Istarska Županija | OIB 65231602058
+T/F 052/741-160 | Fax 052/741-160 | GSM 098/939-4674 <a href="mailto:geoumag@gmail.com">geoumag@gmail.com</a> <br />
+2014 | All rights reserved | developed by <a href="http://www.buonarte.com">BuonArte</a></div>    </td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
---
+++ kontakt.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Geotehnika doo</title>
+<meta name="keywords" content="miniranje, miner, bušenje, eksploziv, proizvodnja, sarajevo, ankerisanje, podvodno, podvodna, prodaja, postrojenje, rušenje, zgrada, mjerenje, seizmička, projektovanje" />
+<link href="css.css" rel="stylesheet" type="text/css" />
+<script src="/js-global/FancyZoom.js" type="text/javascript"></script>
+<script src="/js-global/FancyZoomHTML.js" type="text/javascript"></script>
+</head>
+
+<body onload="setupZoom()">
+<div class="wrap">
+  <table width="940" border="0" align="center" cellpadding="0" cellspacing="0">
+    <tr>
+      <td colspan="2" align="right" valign="bottom" class="header"><div class="headlink1"><a href="index.php">Naslovna</a></div><div class="headlink2"><a href="kontakt.php">Kontakt</a></div></td>
+    </tr>
+    <tr>
+      <td colspan="2" class="main"><table width="900" border="0" align="center" cellpadding="0" cellspacing="0">
+        <tr>
+          <td width="256" align="center" valign="top" ><table width="256" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+              <td class="bgr"><div class="poslovi">PODMORSKA MINIRANJA </div></td>
+            </tr>
+            <tr>
+              <td class="bgr"><div class="poslovi">PODMORSKI ISKOPI</div></td>
+            </tr>
+            <tr>
+              <td class="bgr"><div class="poslovi">BUŠENJA I MINIRANJA NA KOPNU</div></td>
+            </tr>
+          </table></td>
+          <td width="684" align="center" valign="middle"><?php include 'galk1.html'; ?></td>
+        </tr>
+      </table></td>
+    </tr>
+    <tr>
+      <td width="242" align="left" valign="top"><br />
+        <h1><br />
+        Kontakt info</h1>
+        Geotehnika  d.o.o. Umag<br />
+Tribje 26<br />
+52470 Umag<br />
+Istarska Županija <br />
+OIB 65231602058<br />
+<br />
+T/F 052/741-160<br />
+Fax 052/741-160<br />
+GSM 098/939-4674<br />
+<a href="mailto:geoumag@gmail.com">geoumag@gmail.com</a> </a><br />
+<br />
+      <br /></td>
+      <td width="698"><br /><br /><br /><iframe width="650" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=13.517700433731079%2C45.4262718416104%2C13.529287576675415%2C45.43305576696261&amp;layer=mapnik&amp;marker=45.429660141542854%2C13.523494005203247" style="border: 0px solid black"></iframe></td>
+    </tr>
+    <tr>
+      <td colspan="2" align="center" valign="middle"><div class="siva"></div><div class="foottext">Geotehnika  d.o.o. Umag| Tribje 26 | 52470 Umag | Istarska Županija | OIB 65231602058
+T/F 052/741-160 | Fax 052/741-160 | GSM 098/939-4674 <a href="mailto:geoumag@gmail.com">geoumag@gmail.com</a> <br />
+2014 | All rights reserved | developed by BuonArte</div>   </td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
